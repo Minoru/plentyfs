243c9e0775e1849b2c1f8dde4da0a914eea6a323
Lars Wirzenius <liw@liw.fi>
2020-10-18 09:27:32 +0300

    test: rudimentary subplot
    Add a subplot for documenting and verifying acceptance criteria for
    plentyfs. This is a start, and only does a very rudimentary "smoke
    test" to see if plentyfs works at all.

---
+++ subplot/daemon.py
@@ -0,0 +1,77 @@
+#############################################################################
+# Start and stop daemons, or background processes.
+
+
+import logging
+import os
+import signal
+
+
+# Start a process in the background.
+def start_daemon(ctx, name, argv):
+    runcmd_run = globals()["runcmd_run"]
+    runcmd_exit_code_is = globals()["runcmd_exit_code_is"]
+
+    logging.debug(f"Starting daemon {name}")
+    logging.debug(f"  ctx={ctx.as_dict()}")
+    logging.debug(f"  name={name}")
+    logging.debug(f"  argv={argv}")
+
+    if "daemon" not in ctx.as_dict():
+        ctx["daemon"] = {}
+    assert name not in ctx["daemon"]
+    this = ctx["daemon"][name] = {
+        "pid-file": f"{name}.pid",
+        "stderr": f"{name}.stderr",
+        "stdout": f"{name}.stdout",
+    }
+    runcmd_run(
+        ctx,
+        [
+            "/usr/sbin/daemonize",
+            "-c",
+            os.getcwd(),
+            "-p",
+            this["pid-file"],
+            "-e",
+            this["stderr"],
+            "-o",
+            this["stdout"],
+        ]
+        + argv,
+    )
+    runcmd_exit_code_is(ctx, 0)
+    this["pid"] = int(open(this["pid-file"]).read().strip())
+    assert process_exists(this["pid"])
+
+    logging.debug(f"Started daemon {name}")
+    logging.debug(f"  ctx={ctx.as_dict()}")
+
+
+# Stop a daemon.
+def stop_daemon(ctx, name):
+    logging.debug(f"Stopping daemon {name}")
+    logging.debug(f"  ctx={ctx.as_dict()}")
+    logging.debug(f"  ctx['daemon']={ctx.as_dict()['daemon']}")
+
+    this = ctx["daemon"][name]
+    terminate_process(this["pid"], signal.SIGKILL)
+
+
+# Does a process exist?
+def process_exists(pid):
+    try:
+        os.kill(pid, 0)
+    except ProcessLookupError:
+        return False
+    return True
+
+
+# Terminate process.
+def terminate_process(pid, signalno):
+    logging.debug(f"Terminating process {pid} with signal {signalno}")
+    try:
+        os.kill(pid, signalno)
+    except ProcessLookupError:
+        logging.debug("Process did not actually exist (anymore?)")
+        pass
---
+++ subplot/plentyfs.py
@@ -0,0 +1,48 @@
+import logging
+import os
+import time
+
+
+def mount_plentyfs(ctx, dirname=None):
+    logging.info(f"starting plentyfs at {dirname}")
+
+    srcdir = globals()["srcdir"]
+    start_daemon = globals()["start_daemon"]
+
+    os.mkdir(dirname)
+    plentyfs = os.path.join(srcdir, "target", "debug", "plentyfs")
+    start_daemon(ctx, "plentyfs", [plentyfs, dirname])
+
+    # Wait for plentyfs to have started, up to two seconds.
+    started = time.time()
+    timeout = 2.0
+    while time.time() < started + timeout:
+        if os.listdir(dirname):
+            break
+
+    ctx["mount-point"] = dirname
+
+
+def unmount_plentyfs(ctx):
+    runcmd_run = globals()["runcmd_run"]
+    runcmd_exit_code_is = globals()["runcmd_exit_code_is"]
+
+    dirname = ctx["mount-point"]
+    logging.info(f"stopping plentyfs at {dirname}")
+    runcmd_run(ctx, ["fusermount", "-u", dirname])
+    runcmd_exit_code_is(ctx, 0)
+
+
+def file_count_is(ctx, count=None, dirname=None):
+    logging.debug(f"counting files under {dirname}")
+    n = 0
+    for path, subdirs, basenames in os.walk(dirname):
+        logging.debug(f"path: {path}")
+        logging.debug(f"subdirs: {subdirs}")
+        logging.debug(f"basenames: {basenames}")
+        at_path = len(basenames) + len(subdirs)
+        logging.debug(f"under {path}: {at_path}")
+        n += at_path
+
+    assert_eq = globals()["assert_eq"]
+    assert_eq(int(count), n)
